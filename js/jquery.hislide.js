da5a0fb42c99912a177071c10f7540aeced34b2f
kimsehyeon <lovely201253@gmail.com>
2021-01-22 17:24:25 +0900

    v3

--- js/jquery.hislide.js
+++ js/jquery.hislide.js
@@ -17,13 +17,13 @@
         $.extend(true, setting, options);
         // 规定好每张图片处于的位置和状态
         var states = [
-            { $zIndex: 1, width: 120, height: 150, top: 69, left: 134, $opacity: 0.2 },
-            { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 0.4 },
-            { $zIndex: 3, width: 170, height: 218, top: 35, left: 110, $opacity: 0.7 },
-            { $zIndex: 4, width: 224, height: 288, top: 0, left: 263, $opacity: 1 },
-            { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7 },
-            { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.4 },
-            { $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 0.2 }
+            { $zIndex: 1, width: 420, height: 525, top: 69, left: 134, $opacity: 0.2 },
+            { $zIndex: 2, width: 430, height: 562, top: 59, left: 0, $opacity: 0.4 },
+            { $zIndex: 3, width: 470, height: 603, top: 35, left: 110, $opacity: 0.7 },
+            { $zIndex: 4, width: 524, height: 674, top: 0, left: 263, $opacity: 1 },
+            { $zIndex: 3, width: 470, height: 603, top: 35, left: 470, $opacity: 0.7 },
+            { $zIndex: 2, width: 430, height: 562, top: 59, left: 620, $opacity: 0.4 },
+            { $zIndex: 1, width: 420, height: 525, top: 69, left: 500, $opacity: 0.2 }
         ];
 
         var $lis = $ele.find('li');
